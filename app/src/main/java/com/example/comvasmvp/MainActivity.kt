af073555adfc47fc82ab2f6e1865b828bae7419c
koukitsuji <cookieee.626+tech@gmail.com>
2019-08-10 07:01:00 +0900

    Add DB for project

--- app/src/main/java/com/example/comvasmvp/MainActivity.kt
+++ app/src/main/java/com/example/comvasmvp/MainActivity.kt
@@ -5,15 +5,18 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import io.realm.Realm
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        realm = Realm.getDefaultInstance()
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -35,5 +38,10 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
     }
 }
---
+++ app/src/main/java/com/example/comvasmvp/Project.kt
@@ -0,0 +1,13 @@
+package com.example.comvasmvp
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import java.util.*
+
+open class Project : RealmObject() {
+    @PrimaryKey
+    var projectid: Long = 0
+    var date: Date = Date()
+    var title: String = ""
+    var detail: String = ""
+}
