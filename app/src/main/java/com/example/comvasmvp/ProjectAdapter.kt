dad7c93a994fa3af554f8244f4c12f37f30b3049
koukitsuji <cookieee.626+tech@gmail.com>
2019-08-10 07:23:01 +0900

    Add adapter to project DB

---
+++ app/src/main/java/com/example/comvasmvp/ProjectAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.comvasmvp
+
+import android.text.format.DateFormat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import io.realm.OrderedRealmCollection
+import io.realm.RealmBaseAdapter
+
+class ProjectAdapter(data: OrderedRealmCollection<Project>?) : RealmBaseAdapter<Project>(data) {
+
+    inner class ViewHolder (cell: View) {
+        val date = cell.findViewById<TextView>(android.R.id.text1)
+        val title = cell.findViewById<TextView>(android.R.id.text2)
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View
+        val viewHolder: ViewHolder
+
+        when (convertView) {
+            null -> {
+                val inflater = LayoutInflater.from(parent?.context)
+                view = inflater.inflate(android.R.layout.simple_list_item_2, parent, false)
+                viewHolder = ViewHolder(view)
+                view.tag = viewHolder
+            }
+            else -> {
+                view = convertView
+                viewHolder = view.tag as ViewHolder
+            }
+        }
+
+        adapterData?.run {
+            val project = get(position)
+            viewHolder.date.text = DateFormat.format("yyyy/MM/dd", project.date)
+            viewHolder.title.text = project.title
+        }
+        return view
+
+    }
+}
