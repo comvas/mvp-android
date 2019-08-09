e0bd2965e1dcd5c167c63bb5457461bb213c975f
koukitsuji <cookieee.626+tech@gmail.com>
2019-08-10 08:18:06 +0900

    Add project edit activity

---
+++ app/src/main/java/com/example/comvasmvp/ProjectEditActivity.kt
@@ -0,0 +1,12 @@
+package com.example.comvasmvp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProjectEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_project_edit)
+    }
+}
