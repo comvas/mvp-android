4d1de6d42e085e0d84c0d07cc5088611b6c30be0
koukitsuji <cookieee.626+tech@gmail.com>
2019-08-12 12:00:59 +0900

    Modify algo to create fragment in BoardFragment

--- app/src/main/java/com/example/comvasmvp/BoardFragment.kt
+++ app/src/main/java/com/example/comvasmvp/BoardFragment.kt
@@ -7,10 +7,16 @@ import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import io.realm.Realm
+import io.realm.kotlin.where
+import io.realm.RealmResults
+
+
 
 
 class BoardFragment : Fragment() {
 
+    private lateinit var realm: Realm
     private var projectId: Long = 0
 
     companion object {
@@ -47,11 +53,19 @@ class BoardFragment : Fragment() {
     }
 
     private fun setupViewPager(viewPager: ViewPager) {
-        val adapter = CustomPagerAdapter(childFragmentManager)
-        adapter.addFragment(ColorFragment.createInstance("hoge", Color.RED, projectId, 1), "HI-FIVES")
-        adapter.addFragment(ColorFragment.createInstance("hoge", Color.BLUE, projectId, 2), "HI-FIVES")
-        adapter.addFragment(ColorFragment.createInstance("hoge", Color.RED, projectId, 3), "HI-FIVES")
-        viewPager.adapter = adapter
+        realm = Realm.getDefaultInstance()
+        val project = realm.where<Project>().equalTo("projectId", projectId).findFirst()
+        val boards = project?.boardList?.where()?.findAll()
 
+        // 本当は動的に実装すべき
+        // MVPでは3つ決めうち、かつ数に変更なしなので以下の実装をしている
+        // しかし、実際の環境ではボード数と順番は変化するため、変更が必要
+        // １，boardListのインスタンスにおいて、priorityでソート
+        // ２，forで回しながら、プライオリティ昇順にfragmentを生成していく
+        val adapter = CustomPagerAdapter(childFragmentManager)
+        adapter.addFragment(ColorFragment.createInstance("hoge", Color.RED, projectId, boards?.get(0)!!.boardId), "HI-FIVES")
+        adapter.addFragment(ColorFragment.createInstance("hoge", Color.BLUE, projectId, boards?.get(1)!!.boardId), "HI-FIVES")
+        adapter.addFragment(ColorFragment.createInstance("hoge", Color.RED, projectId, boards?.get(2)!!.boardId), "HI-FIVES")
+        viewPager.adapter = adapter
     }
 }
