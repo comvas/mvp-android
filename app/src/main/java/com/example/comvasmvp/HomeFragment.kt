c91e7da8a6b68d80379c4357ca9e97df82f0987b
koukitsuji <cookieee.626+tech@gmail.com>
2019-08-13 19:15:08 +0900

    Add view to show remaining days and time

--- app/src/main/java/com/example/comvasmvp/HomeFragment.kt
+++ app/src/main/java/com/example/comvasmvp/HomeFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import io.realm.Realm
 import io.realm.kotlin.where
+import java.util.*
 
 
 class HomeFragment : Fragment() {
@@ -47,9 +48,44 @@ class HomeFragment : Fragment() {
         realm = Realm.getDefaultInstance()
         val project = realm.where<Project>().equalTo("projectId", projectId).findFirst()
 
-        var projectTitleTextView: TextView = view.findViewById(R.id.projectTitleText)
-        var projectDateTextView: TextView = view.findViewById(R.id.projectDateText)
+        // ヘッダに目標と期日表示
+        val projectTitleTextView: TextView = view.findViewById(R.id.projectTitleText)
+        val projectDateTextView: TextView = view.findViewById(R.id.projectDateText)
         projectTitleTextView.setText(project?.title)
         projectDateTextView.setText(DateFormat.format("yyyy/MM/dd", project?.date).toString())
+
+        // 残り日数を計算
+        val remainingDate = dateDiff(Date(), project!!.date)
+        val projectRemainingDateTextView: TextView = view.findViewById(R.id.projectRemainingDateText)
+        projectRemainingDateTextView.setText("残り日数： " + remainingDate.toString() + "日")
+
+        // 残り時間を計算
+        val projectRemainingTimeTextView: TextView = view.findViewById(R.id.projectRemainingTimeText)
+        projectRemainingTimeTextView.setText("残り時間(推定)： " + (remainingDate * 3).toString() + "時間 (1日当たり3.0時間計算)")
+
+
+    }
+
+    // 日にちの差分を計算
+    fun dateDiff(dateFrom: Date, dateTo: Date): Int {
+//        val sdf = SimpleDateFormat("yyyy/MM/dd")
+//        var dateTo: Date? = null
+//        var dateFrom: Date? = null
+//
+//        // Date型に変換
+//        try {
+//            dateFrom = sdf.parse(dateFromStrig)
+//            dateTo = sdf.parse(dateToString)
+//        } catch (e: java.text.ParseException) {
+//            e.printStackTrace()
+//        }
+
+        // 差分の日数を計算する
+        val dateTimeTo = dateTo!!.getTime()
+        val dateTimeFrom = dateFrom!!.getTime()
+        val dayDiff = (dateTimeTo - dateTimeFrom) / (1000 * 60 * 60 * 24)
+
+        //println("差分日数 : $dayDiff")
+        return dayDiff.toInt()
     }
 }
